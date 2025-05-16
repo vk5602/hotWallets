574f1ac0c6baf93e7344a43fa60a47c8e903aa2c
vk5602 <72371093+vk5602@users.noreply.github.com>
2025-05-16 11:08:56 +0530

    Update account-temp.js

--- account-temp.js
+++ account-temp.js
@@ -1,4 +1,4 @@
 export const acc = [
-    "vishwajeet_fx.tg|fame always december virus walnut dream fever ecology wisdom vibrant find life|3",
+    "vishwajeet_fx.tg|fame always december virus walnut dream fever|3",
     "fx5602.tg|front tennis love insect cart stadium glove palace meat area rail exchange|2"
 ];
